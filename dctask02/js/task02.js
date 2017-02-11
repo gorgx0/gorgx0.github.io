31fa9cae7d55859ea9475893f1b2401bbe43d35b
Marcin Krasowski <m.krasowski@cinema-city.pl>
2017-02-12 00:52:35 +0100

    added poster support (timeout)

--- dctask02/js/task02.js
+++ dctask02/js/task02.js
@@ -5,11 +5,28 @@ function displayData(data) {
     $.each(data.Movies,function (count, movie) {
         tableBody.append(`<tr data-toggle="collapse" data-target="#movie${count+1}"><td>${count+1}</td><td>${movie.Title}</td></tr>`);
         var propertyLines = '';
+        var poster = '';
         $.each(Object.keys(movie), function (count, propertyName) {
-            if(propertyName!='Title')
-                propertyLines+=`<li>${propertyName} - ${movie[propertyName]}</li>`
+            if(propertyName!='Title'){
+                if(propertyName==='Poster'){
+                    poster = `<img src="${movie[propertyName]}">`;
+                }else{
+                    propertyLines+=`<li>${propertyName} - ${movie[propertyName]}</li>`
+                }
+            }
         });
-        tableBody.append(`<tr><td class="hidderRow" colspan="2"><div class="accordian-body collapse" id="movie${count+1}"><ul>${propertyLines}</ul></div></td></tr>`);
+        tableBody.append(`<tr>
+                            <td class="hidderRow" colspan="2">
+                                <div class="accordian-body collapse" id="movie${count+1}">
+                                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                                        <ul>${propertyLines}</ul>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                                        ${poster}						
+                                    </div>
+                                </div>
+                            </td>
+                         </tr>`);
     })
 }
 
